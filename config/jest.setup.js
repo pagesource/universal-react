024bb4e249ce949918b3aeef34b8b601b18818fd
Amrit Mishra <amishra67@sapient.com>
2019-04-16 22:20:56 +0530

    fixed failing test case

--- config/jest.setup.js
+++ config/jest.setup.js
@@ -1,7 +1,12 @@
 import { setConfig } from 'next/config';
-import config from '../app/next.config';
 
-setConfig(config.publicRuntimeConfig);
+const publicRuntimeConfig = {
+  metricsKey: process.env.ENV_API_KEY === process.env.PROD_KEY ? 'prod' : 'dev',
+  isCachingEnabled: process.env.CACHE_ENABLED !== 'false',
+  isProd: process.env.PROD_ENV === 'true',
+};
+
+setConfig(publicRuntimeConfig);
 
 const { configure } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
