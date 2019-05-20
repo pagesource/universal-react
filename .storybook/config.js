fe88df540e9b858369b9bdef0e9403ff12ee758b
vinkumar2 <vinkumar2@publicisgroupe.net>
2019-05-20 22:59:13 +0530

    Fixed the Storybook configurations in Universal react

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,34 +1,27 @@
 import 'react-app-polyfill/ie11';
 import React from 'react';
 import { configure, setAddon, addDecorator } from '@storybook/react';
-import { setOptions } from '@storybook/addon-options';
-import chaptersAddon, { setDefaults } from 'react-storybook-addon-chapters';
+import { withOptions } from '@storybook/addon-options';
 import { withConsole } from '@storybook/addon-console';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withSmartKnobs } from 'storybook-addon-smart-knobs';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../app/styles/theme';
 import '../app/styles/cssIncludes';
 import '../app/styles';
 
-setOptions({
+withOptions({
   name: `Universal React`,
   url:
     'https://tools.publicis.sapient.com/bitbucket-code-commons/projects/XTI/repos/universal-react/browse',
 });
 
-setDefaults({
-  sectionOptions: {
-    showSource: false,
-    allowSourceToggling: false,
-    showPropTables: false,
-    allowPropTablesToggling: false,
-  },
-});
+addDecorator(withKnobs);
+addDecorator(withSmartKnobs);
 
 addDecorator((storyFn, context) => (
   <ThemeProvider theme={Theme}>{withConsole()(storyFn)(context)}</ThemeProvider>
 ));
-
-setAddon(chaptersAddon);
 
 const req = require.context('../app/components', true, /story\.js$/);
 
--- app/components/molecules/TopBanner/TopBanner.story.js
+++ app/components/molecules/TopBanner/TopBanner.story.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
 import TopBanner from './index';
 
-storiesOf('TopBanner', module).add('', () => <TopBanner>Hello TopBanner</TopBanner>);
+storiesOf('TopBanner', module).add('TopBanner', () => <TopBanner>Hello TopBanner</TopBanner>);
